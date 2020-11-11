809953bf5b7f002592256371d2dfb02f45d0acd2
Mohita <mohita.sindri@gmail.com>
2020-11-11 18:33:54 +0530

    merge from main

---
+++ example.py
@@ -0,0 +1,25 @@
+# python = ['cool']
+# x = python in python
+
+# print(x)
+
+
+print('''\
+ A
+ B
+ C
+ ''')
+
+x = 15
+y = 8
+if((x > 3) or (y/2)):
+    print('At least one of the conditions is satisfied.')
+else:
+    print('Neither condition is satisfied.')
+
+if(x > 3):
+    if(y > 2):
+        print('At least one of the conditions is satisfied.')
+
+else:
+    print('Neither condition is satisfied.')
