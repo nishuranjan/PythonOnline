9b999ef0ce200fcb4355dd87dc8d7f11f82520b4
Mohita <mohita.sindri@gmail.com>
2020-11-12 22:34:11 +0530

    task added

---
+++ Day-1/05. Assignments/test.py
@@ -0,0 +1,13 @@
+# # string = 'ppy!'
+# # fruit = 'a'.join(list(string))
+# # print(fruit)
+
+# # x = 55 / 11
+# # print(x)
+
+# x='P"yt\'h"on'
+# print(x)# start with single quotes 
+# #that's why it back slace the single qoutes into the string 
+e = 31
+e = float(e)
+print(e)
--- example.py
+++ example.py
@@ -1,25 +1,81 @@
-# python = ['cool']
-# x = python in python
+# # # # # # # # # python = ['cool']
+# # # # # # # # # x = python in python
 
-# print(x)
+# # # # # # # # # print(x)
 
 
-print('''\
- A
- B
- C
- ''')
+# # # # # # # # print('''\
+# # # # # # # #  A
+# # # # # # # #  B
+# # # # # # # #  C
+# # # # # # # #  ''')
+# # # # # # # length = int(input("enter the length of the rectangle \n"))
+# # # # # # # breadth = int(input('enter the breadth of the rectangle \n'))
 
-x = 15
-y = 8
-if((x > 3) or (y/2)):
-    print('At least one of the conditions is satisfied.')
+# # # # # # # if length == breadth:
+# # # # # # #     print("it is a sqaure")
+# # # # # # # else:
+# # # # # # #     print("it is not sqaure")
+# # # # # # a = int(input("enter the number \n"))
+# # # # # # b = int(input("enter the number \n"))
+
+# # # # # # if a > b:
+# # # # # #     print("a is greater")
+# # # # # # else:
+# # # # # #     print("b is greatest")
+# # # # # quantity = int(input('enter the quantity'))
+# # # # # if quantity * 100 > 1000:
+# # # # #     purchasecost1 = (quantity * 100) - (.1 * quantity * 100)
+# # # # #     print(f"price with discount {purchasecost1}")
+# # # # # else:
+# # # # #     purchasecost2 = quantity * 100
+# # # # #     print(f"no discount and ypur amount is {purchasecost2}")
+# # # # salary = int(input("enter the salary"))
+# # # # year = int(input("enter the year"))
+# # # # if year > 5:
+# # # #     netamount = (salary + (0.5 * salary))
+# # # #     print(f" congratulation!!! {netamount}")
+# # # # else:
+# # # #     print(f"Good luck for the future {salary}")
+# # # marks = int(input("enter the marks"))
+
+# # # if marks > 80:
+# # #     print("A")
+# # # elif marks < 80 and marks >= 60:
+# # #     print("B")
+# # # elif marks < 60 and marks >= 50:
+# # #     print("C")
+# # # elif marks < 50 and marks >= 45:
+# # #     print("D")
+# # # elif marks < 45 and marks >= 25:
+# # #     print("E")
+# # # else:
+# # #     print("F")
+# # p1 = int(input("enter the age of first person"))
+# # p2 = int(input("enter the age of second person"))
+# # p3 = int(input("enter the age of third person"))
+
+# # if p1 > p2 and p1 > p3:
+# #     print("p1 is oldest person")
+# # elif p2 > p3:
+# #     print("p2 is oldest")
+# # else:
+# #     print("p3 is oldest")
+# x = int(input("enter the first number"))
+# if x < 0:
+#     output = x * -1
+#     print(output)
+# else:
+#     output = x 
+#     print(output)
+tcls = int(input("enter the total days"))
+atcls = int(input("enter the attended days"))
+medi_cause = input("medical reason")
+atclsper = ((atcls // tcls)*100)
+if medi_cause == 'Y':
+    print("you are allowed")
 else:
-    print('Neither condition is satisfied.')
-
-if(x > 3):
-    if(y > 2):
-        print('At least one of the conditions is satisfied.')
-
-else:
-    print('Neither condition is satisfied.')
+    if atclsper >= 75:
+        print("you are allowed to seat")
+    else:
+        print("you are not  allowed")
