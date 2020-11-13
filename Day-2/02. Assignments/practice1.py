c97f2fae7075b134d4a4af890f9f9a45bb0843fc
Mohita <mohita.sindri@gmail.com>
2020-11-13 15:47:32 +0530

    excercise practice

---
+++ Day-2/02. Assignments/practice1.py
@@ -0,0 +1,20 @@
+# # age = int(input("enter the age"))
+# # sex = input("enter the sex either \'M' or \'F'")
+# # mar_status = input("enter the marital status either \'Y' or \'N'")
+
+# # if sex =='F' and age >= 20 and age <= 60:
+# #     print("she will work only in urban areas")
+# # elif sex == "M" and age >= 20 and age<=60:
+# #     print("he can work in anywhere")
+# # elif sex == "M" and age >= 40 and age <=60:
+# #     print("he will work in urban areas only")
+# # else:
+# #     print("ERROR")
+
+# year= int(input("enter the year"))
+
+# if year % 4 == 0 or (year % 100 == 0 and year % 400 == 0):
+#     print(f"{year} It is a leap year")
+# else:
+#     print(f"{year} is not a leap year")
+
--- Day-3/02. Assignments/Assignments.md
+++ Day-3/02. Assignments/Assignments.md
@@ -4,5 +4,5 @@ Try out to answer the questions here....
 
 https://app.finxter.com/learn/computer/science/
 https://www.afterhoursprogramming.com/tests/python/
-https://pythonspot.com/python-tests-quizes/
-https://www.pythonchecker.com/
+h#ttps://pythonspot.com/python-tests-quizes/
+#https://www.pythonchecker.com/
